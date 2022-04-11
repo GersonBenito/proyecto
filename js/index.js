df2d7bcb346aaff7f8e2d5ec13aa74db55a3ff02
GersonBenito <gersonbenito4@gmail.com>
2022-04-10 18:03:41 -0600

    feat(index.html, index.js, style.css) se agrego validacion del formulario y captura de imagenes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-
 const contendorCards = document.querySelector('.contendor-cards');
 const vacio = document.querySelector('.vacio');
+const alertMessage = document.querySelector('.alert');
 const formulario = document.querySelector('.formulario');
 const titulo = document.querySelector('#titulo'),
       imagen = document.querySelector('#imagen'),
@@ -9,23 +9,33 @@ const titulo = document.querySelector('#titulo'),
       descripcion = document.querySelector('#descripcion');
 
 formulario.style.display = 'none';
+alertMessage.style.display = 'none'
 
 let viajes = [];
+let fileBase64 = '';
 
 const nuevo = () => {
     formulario.style.display = 'flex'
 }
 
+imagen.addEventListener('change',({ target }) =>{
+    convertToBase64(target.files[0]);
+})
+
 const agregarDatos = () =>{
     contendorCards.innerHTML = '';
     const viaje = {
         titulo: titulo.value,
-        imagen: imagen.value,
+        imagen: fileBase64,
         precio: calcualarIVA(precio.value),
         destino: destino.value,
         descripcion: descripcion.value,
     };
 
+    if(!validateForm(viaje)){
+        showAlert('Uno o mas campos estan vacios!');
+        return false;
+    }
     viajes = [viaje, ...viajes];
     generarCard(viajes);
     limpiarFormulario();
@@ -97,6 +107,52 @@ const eliminarViaje = (id) => {
     const resultado = viajes.filter((_, index) => index !== id );
     viajes = [];
     generarCard(resultado);
+}
+
+const validateForm = ({ titulo, imagen, precio, destino, descripcion }) =>{
+    console.log(titulo, precio, destino, descripcion);
+    if(titulo.trim() === '' || imagen.trim() === '' || precio.trim() === '' || destino.trim() == '' || descripcion.trim() === ''){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+const convertToBase64 = (file) =>{
+    let render = new FileReader();
+    render.readAsDataURL(file);
+    render.onloadend = () =>{
+        fileBase64 = render.result;
+    }
+}
+
+const showAlert = (text) =>{
+    alertMessage.style.display = 'flex';
+    let textAlert = document.createElement('p');
+    textAlert.textContent = text;
+    alertMessage.append(textAlert);
+    showAlterInput(true);
+    setTimeout(() => {
+        textAlert.textContent = '';
+        alertMessage.style.display = 'none';
+        showAlterInput(false);
+    }, 2000)
+}
+
+const showAlterInput = (show) =>{
+    if(show){
+        titulo.style.border = '1px solid red';
+        imagen.style.border = '1px solid red';
+        precio.style.border = '1px solid red';
+        destino.style.border = '1px solid red';
+        descripcion.style.border = '1px solid red'
+    }else{
+        titulo.style.border = '';
+        imagen.style.border = '';
+        precio.style.border = '';
+        destino.style.border = '';
+        descripcion.style.border = ''
+    }
 }
 
 
